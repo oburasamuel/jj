71eea832e93c5b8940e90bab350e7e95abb46117
Sam <oburasamuel4@gmail.com>
2024-10-19 22:35:02 +0300

    closures

--- __master__.py
+++ __master__.py
@@ -48,3 +48,17 @@ else:
     print(number, "is not a prime number")
 
 
+# Duck typing
+
+class Duck:
+    def quack(self):
+        print("Quack!")
+
+class Dog:
+    def quack(self):
+        print("I am a dog but I can quack")
+
+def make_it_quack(duck):
+    duck.quack()
+
+make_it_quack(Dog())
